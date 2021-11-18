f8c8760b8371dfae8b8e0e857848bac0098a070a
jhonatanantunes <jhonatan.antunes@kron.digital>
2021-11-17 22:50:33 -0300

    feat: adicionar handlers para SystemMenu

---
+++ src/Comrade.Core/SystemMenuCore/Handlers/SystemMenuCreateCoreHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using Comrade.Core.SystemMenuCore.Commands;
+using Comrade.Core.Bases.Interfaces;
+using Comrade.Core.Bases.Results;
+using Comrade.Core.Messages;
+using Comrade.Domain.Bases;
+using Comrade.Domain.Extensions;
+using MediatR;
+using Comrade.Core.SystemMenuCore.Validations;
+
+namespace Comrade.Core.SystemMenuCore.Handlers;
+
+public class
+    SystemMenuCreateCoreHandler : IRequestHandler<SystemMenuCreateCommand, ISingleResult<Entity>>
+{
+    private readonly SystemMenuCreateValidation _systemMenuCreateValidation;
+    private readonly IMongoDbCommandContext _mongoDbContext;
+    private readonly ISystemMenuRepository _repository;
+
+    public SystemMenuCreateCoreHandler(SystemMenuCreateValidation systemMenuCreateValidation,
+        ISystemMenuRepository repository, IMongoDbCommandContext mongoDbContext)
+    {
+        _systemMenuCreateValidation = systemMenuCreateValidation;
+        _repository = repository;
+        _mongoDbContext = mongoDbContext;
+    }
+
+    public async Task<ISingleResult<Entity>> Handle(SystemMenuCreateCommand request,
+        CancellationToken cancellationToken)
+    {
+        var validate = await _systemMenuCreateValidation.Execute(request).ConfigureAwait(false);
+        if (!validate.Success)
+        {
+            return validate;
+        }
+
+        await _repository.BeginTransactionAsync().ConfigureAwait(false);
+        await _repository.Add(request).ConfigureAwait(false);
+        await _repository.CommitTransactionAsync().ConfigureAwait(false);
+
+        _mongoDbContext.InsertOne(request);
+
+        return new CreateResult<Entity>(true,
+            BusinessMessage.MSG01);
+    }
+}
---
+++ src/Comrade.Core/SystemMenuCore/Handlers/SystemMenuDeleteCoreHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using Comrade.Core.SystemMenuCore.Commands;
+using Comrade.Core.Bases.Interfaces;
+using Comrade.Core.Bases.Results;
+using Comrade.Core.Messages;
+using Comrade.Domain.Bases;
+using Comrade.Domain.Models;
+using MediatR;
+using Comrade.Core.SystemMenuCore.Validations;
+
+namespace Comrade.Core.SystemMenuCore.Handlers;
+
+public class
+    SystemMenuDeleteCoreHandler : IRequestHandler<SystemMenuDeleteCommand, ISingleResult<Entity>>
+{
+    private readonly SystemMenuDeleteValidation _systemMenuDeleteValidation;
+    private readonly IMongoDbCommandContext _mongoDbContext;
+    private readonly ISystemMenuRepository _repository;
+
+    public SystemMenuDeleteCoreHandler(SystemMenuDeleteValidation systemMenuDeleteValidation,
+        ISystemMenuRepository repository, IMongoDbCommandContext mongoDbContext)
+    {
+        _systemMenuDeleteValidation = systemMenuDeleteValidation;
+        _repository = repository;
+        _mongoDbContext = mongoDbContext;
+    }
+
+    public async Task<ISingleResult<Entity>> Handle(SystemMenuDeleteCommand request,
+        CancellationToken cancellationToken)
+    {
+        var recordExists = await _repository.GetById(request.Id).ConfigureAwait(false);
+
+        if (recordExists is null)
+        {
+            return new DeleteResult<Entity>(false,
+                BusinessMessage.MSG04);
+        }
+
+        var validate = _systemMenuDeleteValidation.Execute(recordExists);
+        if (!validate.Success)
+        {
+            return validate;
+        }
+
+        var systemMenuId = recordExists.Id;
+        _repository.Remove(systemMenuId);
+
+        await _repository.BeginTransactionAsync().ConfigureAwait(false);
+        _repository.Remove(systemMenuId);
+        await _repository.CommitTransactionAsync().ConfigureAwait(false);
+
+        _mongoDbContext.DeleteOne<SystemMenu>(systemMenuId);
+
+        return new DeleteResult<Entity>(true,
+            BusinessMessage.MSG03);
+    }
+}
