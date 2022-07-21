7ef642d986739d843740aa2eabdf1e3113d8d57a
jhonatan izidoro <jhonatanizidoro@gmail.com>
2022-07-21 18:24:51 -0300

    removing unused libs

--- src/Comrade.Api/Modules/Common/LoggingExtensions.cs
+++ src/Comrade.Api/Modules/Common/LoggingExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Serilog.Extensions.Logging;
-using Serilog.Sinks.MSSqlServer;
 
 namespace Comrade.Api.Modules.Common;
 
@@ -13,35 +12,6 @@ public static class LoggingExtensions
             .MinimumLevel.Information()
             .Enrich.With(new ApplicationDetailsEnricher())
             .Enrich.FromLogContext()
-            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-            .WriteTo.MongoDB("mongodb://localhost/local")
-            .WriteTo.Providers(providers)
-            .CreateLogger();
-    }
-
-    public static void CreateLogSqlServer(LoggerProviderCollection providers,
-        IConfigurationRoot configurationRoot)
-    {
-        var connection = configurationRoot.GetValue<string>("ConnectionStrings:MsSqlDbConnection");
-
-        var columnOptions = new ColumnOptions
-        {
-            AdditionalColumns = new Collection<SqlColumn>
-            {
-                new("UserName", SqlDbType.VarChar)
-            }
-        };
-
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .Enrich.With(new ApplicationDetailsEnricher())
-            .Enrich.FromLogContext()
-            .WriteTo.MSSqlServer(connection,
-                new MSSqlServerSinkOptions
-                {
-                    AutoCreateSqlTable = true,
-                    TableName = "log_auth_user_service"
-                }, columnOptions: columnOptions)
             .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
             .WriteTo.MongoDB("mongodb://localhost/local")
             .WriteTo.Providers(providers)
