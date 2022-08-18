baf149fe3033b91789817fea934fa3724f6a6c67
Lucas Borges de Moraes <lucasborges1312@icloud.com>
2022-08-18 20:33:09 -0300

    Criação dos testes automatizados.

---
+++ tests/Comrade.IntegrationTests/Tests/SystemRoleIntegrationTests/SystemRoleControllerEditErrorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Comrade.Application.Bases;
+using Comrade.Application.Components.SystemRoleComponent.Contracts;
+using Comrade.Persistence.Repositories;
+using Comrade.UnitTests.DataInjectors;
+using Comrade.UnitTests.Tests.SystemRoleTests.Bases;
+using Xunit;
+
+namespace Comrade.IntegrationTests.Tests.SystemRoleIntegrationTests;
+
+public class SystemRoleControllerEditErrorTests : IClassFixture<ServiceProviderFixture>
+{
+    private readonly ServiceProviderFixture _fixture;
+
+    public SystemRoleControllerEditErrorTests(ServiceProviderFixture fixture)
+    {
+        _fixture = fixture;
+        InjectDataOnContextBase.InitializeDbForTests(_fixture.SqlContextFixture);
+    }
+
+    [Fact]
+    public async Task SystemRoleController_Edit_Error()
+    {
+        var id = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+        var testObject = new SystemRoleEditDto
+        {
+            Id = id,
+            Name = null
+        };
+        var controller = SystemRoleInjectionController.GetSystemRoleController(_fixture.SqlContextFixture, _fixture.MongoDbContextFixture, _fixture.Mediator);
+        var result = await controller.Edit(testObject);
+        if (result is ObjectResult okResult)
+        {
+            var actualResultValue = okResult.Value as SingleResultDto<EntityDto>;
+            Assert.NotNull(actualResultValue);
+            Assert.Equal(409, actualResultValue?.Code);
+        }
+        var repository = new SystemRoleRepository(_fixture.SqlContextFixture);
+        var user = await repository.GetById(id);
+        Assert.NotNull(user!.Name);
+    }
+}
---
+++ tests/Comrade.UnitTests/Tests/SystemRoleTests/Bases/SystemRoleInjectionController.cs
@@ -0,0 +1,17 @@
+﻿using Comrade.Api.Controllers.V1.SystemRoleApi;
+using Comrade.Persistence.DataAccess;
+using Comrade.UnitTests.Helpers;
+using MediatR;
+
+namespace Comrade.UnitTests.Tests.SystemRoleTests.Bases;
+
+public class SystemRoleInjectionController
+{
+    public static SystemRoleController GetSystemRoleController(ComradeContext context, MongoDbContext mongoDbContextFixture, IMediator mediator)
+    {
+        var mapper  = MapperHelper.ConfigMapper();
+        var command = SystemRoleInjectionService.GetSystemRoleCommand(context, mediator);
+        var query   = SystemRoleInjectionService.GetSystemRoleQuery(context, mongoDbContextFixture, mapper);
+        return new SystemRoleController(command, query);
+    }
+}
