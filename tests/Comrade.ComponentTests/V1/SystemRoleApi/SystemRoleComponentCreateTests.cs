aadc3606a3c062358e070595fe6604aa2778c0a2
Lucas Borges de Moraes <lucasborges1312@icloud.com>
2022-08-23 19:08:42 -0300

    Testes integrados/unitarios/componente da nova validação criada.

---
+++ tests/Comrade.ComponentTests/V1/SystemRoleApi/SystemRoleComponentCreateTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text;
+using Comrade.Core.Messages;
+using Comrade.Domain.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Comrade.ComponentTests.V1.SystemRoleApi;
+
+public class SystemRoleComponentCreateTests : IClassFixture<CustomWebApplicationFactoryFixture>
+{
+    [Fact]
+    public async Task CreateSystemRole()
+    {
+        var fixture = new CustomWebApplicationFactoryFixture();
+        var client = fixture.CustomWebApplicationFactory.CreateClient();
+        var token = await GenerateFakeToken.Execute(fixture.Mediator);
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new SystemRole("ROLE")), Encoding.UTF8);
+        httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var actualResponse = await client.PostAsync("/api/v1/system-role/create", httpContent).ConfigureAwait(false);
+        var actualResponseString = await actualResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        Assert.Equal(HttpStatusCode.Created, actualResponse.StatusCode);
+
+        using StringReader stringReader = new(actualResponseString);
+        using JsonTextReader reader = new(stringReader) { DateParseHandling = DateParseHandling.None };
+        var jsonResponse = await JObject.LoadAsync(reader).ConfigureAwait(false);
+
+        Assert.Equal(BusinessMessage.MSG01, jsonResponse["message"]);
+    }
+}
