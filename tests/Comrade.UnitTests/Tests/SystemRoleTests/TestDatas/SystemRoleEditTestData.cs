d989021c40c763c892f629cd097192a82c4731e1
Lucas Borges de Moraes <lucasborges1312@icloud.com>
2022-08-23 19:08:42 -0300

    Testes integrados/unitarios/componente da nova validação criada.

--- tests/Comrade.UnitTests/Tests/SystemRoleTests/TestDatas/SystemRoleEditTestData.cs
+++ tests/Comrade.UnitTests/Tests/SystemRoleTests/TestDatas/SystemRoleEditTestData.cs
@@ -1,4 +1,5 @@
 ﻿using Comrade.Core.SystemRoleCore.Commands;
+using Comrade.Domain.Enums;
 
 namespace Comrade.UnitTests.Tests.SystemRoleTests.TestDatas;
 
@@ -8,7 +9,7 @@ internal class SystemRoleEditTestData : IEnumerable<object[]>
     {
         yield return new object[]
         {
-            204,
+            EnumResponse.NoContent,
             new SystemRoleEditCommand
             {
                 Id = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
@@ -17,10 +18,20 @@ internal class SystemRoleEditTestData : IEnumerable<object[]>
         };
         yield return new object[]
         {
-            404,
+            EnumResponse.NotFound,
             new SystemRoleEditCommand
             {
-                Id = new Guid("00000000-1b83-46f2-91eb-0c64f1c638a7")
+                Id = new Guid("00000000-1b83-46f2-91eb-0c64f1c638a7"),
+                Name = "ROLE"
+            }
+        };
+        yield return new object[]
+        {
+            EnumResponse.ErrorBusinessValidation,
+            new SystemRoleEditCommand
+            {
+                Id = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                Name = " aDm "
             }
         };
     }
