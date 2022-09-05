780bccda9ebdab73d11f0a92caf4979f3b3ada7e
Lucas Borges de Moraes <lucasborges1312@icloud.com>
2022-09-05 19:31:06 -0300

    Arrumar a chave estrangeira da tabela User-Roles.

--- src/Comrade.Domain/Models/SystemRole.cs
+++ src/Comrade.Domain/Models/SystemRole.cs
@@ -8,16 +8,20 @@ public class SystemRole : Entity
     public SystemRole()
     {
         Name = "";
+        SystemUsers = new HashSet<SystemUser>();
     }
 
     public SystemRole(string name)
     {
         Name = name;
+        SystemUsers = new HashSet<SystemUser>();
     }
     
     [Column("syro_tx_name", TypeName = "varchar")]
     [MaxLength(255)]
     [Required(ErrorMessage = "NOME ROLE is required")]
     public string Name { get; set; } // varchar(255), not null
+
+    public virtual ICollection<SystemUser> SystemUsers { get; set; }
 }
 
