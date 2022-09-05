28ce823d35247318ab4c013e5ef9649b5bfb13d7
Lucas Borges de Moraes <lucasborges1312@icloud.com>
2022-09-05 19:31:06 -0300

    Arrumar a chave estrangeira da tabela User-Roles.

---
+++ src/Comrade.Domain/Models/SystemRole.cs
@@ -0,0 +1,27 @@
+﻿using Comrade.Domain.Bases;
+
+namespace Comrade.Domain.Models;
+
+[Table("syro_system_role")]
+public class SystemRole : Entity
+{
+    public SystemRole()
+    {
+        Name = "";
+        SystemUsers = new HashSet<SystemUser>();
+    }
+
+    public SystemRole(string name)
+    {
+        Name = name;
+        SystemUsers = new HashSet<SystemUser>();
+    }
+    
+    [Column("syro_tx_name", TypeName = "varchar")]
+    [MaxLength(255)]
+    [Required(ErrorMessage = "NOME ROLE is required")]
+    public string Name { get; set; } // varchar(255), not null
+
+    public virtual ICollection<SystemUser> SystemUsers { get; set; }
+}
+
---
+++ src/Comrade.Persistence/Mappings/SystemRoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Comrade.Domain.Models;
+
+namespace Comrade.Persistence.Mappings;
+
+public class SystemRoleConfiguration : IEntityTypeConfiguration<SystemRole>
+{
+    public void Configure(EntityTypeBuilder<SystemRole> builder)
+    {
+        builder.Property(b => b.Id).HasColumnName("syro_uuid_system_role").IsRequired();
+        builder.HasKey(c => c.Id).HasName("pk_syro_system_role");
+
+        builder.HasMany(x => x.SystemUsers)
+            .WithMany(x => x.SystemRoles)
+            .UsingEntity(j => j.ToTable("syus_system_user_syro_system_role"));
+    }
+}
