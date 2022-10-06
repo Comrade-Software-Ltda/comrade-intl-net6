ef2b17ad0c7d77cd13d3b2e04567bd4e7648bb17
abraaoribeiro23 <abraaoribeiroo23@gmail.com>
2022-10-06 01:33:28 -0300

    feat: SystemUser Permissions and Roles Controllers and enable feature flags

---
+++ src/Comrade.Api/Controllers/V1/SystemUserApi/SystemUserSystemPermissionController.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Comrade.Api.Modules.Common;
+using Comrade.Api.Modules.Common.FeatureFlags;
+using Comrade.Application.Bases;
+using Comrade.Application.Components.SystemUserSystemPermissionComponent.Commands;
+using Comrade.Application.Components.SystemUserSystemPermissionComponent.Contracts;
+using Comrade.Application.Components.SystemUserSystemPermissionComponent.Queries;
+using Comrade.Application.Paginations;
+using Microsoft.AspNetCore.Http;
+
+namespace Comrade.Api.Controllers.V1.SystemUserApi;
+
+[FeatureGate(CustomFeature.SystemUserSystemPermission)]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class SystemUserSystemPermissionController : ControllerBase
+{
+    private readonly ISystemUserSystemPermissionCommand _command;
+    private readonly ISystemUserSystemPermissionQuery _query;
+
+    public SystemUserSystemPermissionController(ISystemUserSystemPermissionCommand command,
+        ISystemUserSystemPermissionQuery query)
+    {
+        _command = command;
+        _query = query;
+    }
+
+    [HttpGet("get-all")]
+    [ApiConventionMethod(typeof(CustomApiConventions), nameof(CustomApiConventions.List))]
+    public async Task<IActionResult> GetAll([FromQuery] PaginationQuery? paginationQuery)
+    {
+        try
+        {
+            var result = await _query.GetAll(paginationQuery).ConfigureAwait(false);
+            return StatusCode(result.Code, result);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new SingleResultDto<EntityDto>(e));
+        }
+    }
+
+
+    [HttpPut("manage")]
+    [ApiConventionMethod(typeof(CustomApiConventions), nameof(CustomApiConventions.Edit))]
+    public async Task<IActionResult> Edit([FromBody][Required] SystemUserSystemPermissionManageDto dto)
+    {
+        try
+        {
+            var result = await _command.Manage(dto).ConfigureAwait(false);
+            return StatusCode(result.Code, result);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new SingleResultDto<EntityDto>(e));
+        }
+    }
+}
---
+++ src/Comrade.Api/Controllers/V1/SystemUserApi/SystemUserSystemRoleController.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Comrade.Api.Modules.Common;
+using Comrade.Api.Modules.Common.FeatureFlags;
+using Comrade.Application.Bases;
+using Comrade.Application.Components.SystemUserSystemRoleComponent.Commands;
+using Comrade.Application.Components.SystemUserSystemRoleComponent.Contracts;
+using Comrade.Application.Components.SystemUserSystemRoleComponent.Queries;
+using Comrade.Application.Paginations;
+using Microsoft.AspNetCore.Http;
+
+namespace Comrade.Api.Controllers.V1.SystemUserApi;
+
+[FeatureGate(CustomFeature.SystemUserSystemRole)]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class SystemUserSystemRoleController : ControllerBase
+{
+    private readonly ISystemUserSystemRoleCommand _command;
+    private readonly ISystemUserSystemRoleQuery _query;
+
+    public SystemUserSystemRoleController(ISystemUserSystemRoleCommand command,
+        ISystemUserSystemRoleQuery query)
+    {
+        _command = command;
+        _query = query;
+    }
+
+    [HttpGet("get-all")]
+    [ApiConventionMethod(typeof(CustomApiConventions), nameof(CustomApiConventions.List))]
+    public async Task<IActionResult> GetAll([FromQuery] PaginationQuery? paginationQuery)
+    {
+        try
+        {
+            var result = await _query.GetAll(paginationQuery).ConfigureAwait(false);
+            return StatusCode(result.Code, result);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new SingleResultDto<EntityDto>(e));
+        }
+    }
+
+
+    [HttpPut("manage")]
+    [ApiConventionMethod(typeof(CustomApiConventions), nameof(CustomApiConventions.Edit))]
+    public async Task<IActionResult> Edit([FromBody][Required] SystemUserSystemRoleManageDto dto)
+    {
+        try
+        {
+            var result = await _command.Manage(dto).ConfigureAwait(false);
+            return StatusCode(result.Code, result);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new SingleResultDto<EntityDto>(e));
+        }
+    }
+}
